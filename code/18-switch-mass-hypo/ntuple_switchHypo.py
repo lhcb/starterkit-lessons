b45dcc65bbdb7d380ea512230f6aba0392992e11
Sebastian Neubert <sneubert@lxplus0068.cern.ch>
2015-05-27 13:10:21 +0200

    Removed the FilterDesktop from the example solution It is not needed for substitutePID

--- code/18-switch-mass-hypo/ntuple_switchHypo.py
+++ code/18-switch-mass-hypo/ntuple_switchHypo.py
@@ -7,11 +7,6 @@ stream = 'AllStreams'
 line = 'D2hhCompleteEventPromptDst2D2RSLine'
 tesLoc = '/Event/{0}/Phys/{1}/Particles'.format(stream, line)
 
-# prepare a Filter algorithm to creat a new Selection of particles
-#from Configurables import FilterDesktop
-#alg = FilterDesktop('SelFilter')
-#alg.Code = 'ALL' # we could filter out some data here. For now just select ALL candidates
-
 # get the selection(s) created by the stripping
 from PhysSelPython.Wrappers import Selection
 from PhysSelPython.Wrappers import SelectionSequence
@@ -19,28 +14,24 @@ from PhysSelPython.Wrappers import DataOnDemand
 
 strippingSels = [DataOnDemand(Location=tesLoc)] 
 
-# instantiate a selection that uses the algorithm defined above
-# and gets the stripping selection as input
-#sel = Selection('Dst2D0pi_D0Kpi_Sel', Algorithm=alg, RequiredSelections=strippingSels)
-
 # configure an algorithm to substitute the Kaon in the D0-decay by a second pion 
 from Configurables import SubstitutePID
 Subs = SubstitutePID(
         'MakeD02pipi',
         Code = "DECTREE('[D*(2010)+ -> (D0 -> K- pi+) pi+]CC')",
-        Substitutions = {
+        Substitutions = { # note that SubstitutePID can't handle automatic CC
         'Charm -> (D0 -> ^K- pi+) Meson' : 'pi-', 
         'Charm -> (D0 -> ^K+ pi-) Meson' : 'pi+', 
         }
 )
 
 # create a selection using the substitution algorithm
-selSub = Selection("Dst2D0pi_D02pipi_Sel", Algorithm=Subs, RequiredSelections=[strippingSel])
+selSub = Selection("Dst2D0pi_D02pipi_Sel", Algorithm=Subs, RequiredSelections=strippingSels)
 # in order to add the selection into the program make a sequence
 selSeq = SelectionSequence('SelSeq', TopSelection=selSub)
 
 
-# Create an ntuple to capture D*+ decays from the StrippingLine line
+# Create an ntuple to capture D*+ decays from the new selection
 dtt = DecayTreeTuple('TupleDstToD0pi_D0Topipi')
 dtt.Inputs = [selSeq.outputLocation()]
 dtt.Decay = '[D*(2010)+ -> ^(D0 -> ^pi- ^pi+) ^pi+]CC'
@@ -53,7 +44,6 @@ seq.Members += [selSeq.sequence()]
 seq.Members += [dtt]
 DaVinci().appendToMainSequence([seq])
 
-DaVinci().UserAlgorithms += [dtt]
 DaVinci().InputType = 'DST'
 DaVinci().TupleFile = 'DVntuple.root'
 DaVinci().PrintFreq = 1000
