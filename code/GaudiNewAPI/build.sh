8890d16dec01c2436606eae8ed644079642243b4
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-01-24 09:03:49 -0500

    simpify build

--- code/GaudiNewAPI/build.sh
+++ code/GaudiNewAPI/build.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Executing:"
-echo ' export CMTPROJECTPATH=/cvmfs/lhcbdev.cern.ch/nightlies/lhcb-future/Today:$CMTPROJECTPATH'
 echo " lb-project-init"
 echo " make $@"
 echo " ln -s build*/run run"
 echo
-
-export CMTPROJECTPATH=/cvmfs/lhcbdev.cern.ch/nightlies/lhcb-future/Today:$CMTPROJECTPATH
-export CMTPROJECTPATH=$HOME/hackathon:$CMTPROJECTPATH
 
 if [[ ! -f Makefile ]]; then
 lb-project-init
