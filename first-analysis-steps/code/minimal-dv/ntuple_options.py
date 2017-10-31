87c8b628378539b5206a18450e6ca5e8e5f34f68
Alex Pearce <alex@alexpearce.me>
2017-10-31 20:48:21 +0000

    Rebuild pages at f8ecb1b

--- first-analysis-steps/code/minimal-dv/ntuple_options.py
+++ first-analysis-steps/code/minimal-dv/ntuple_options.py
@@ -22,8 +22,8 @@ DaVinci().Simulation = True
 # Only ask for luminosity information when not using simulated data
 DaVinci().Lumi = not DaVinci().Simulation
 DaVinci().EvtMax = -1
-DaVinci().CondDBtag = 'sim-20170721-1-vc-mu100'
-DaVinci().DDDBtag = 'dddb-20171005-3'
+DaVinci().CondDBtag = 'sim-20161124-2-vc-md100'
+DaVinci().DDDBtag = 'dddb-20150724'
 
 # Use the local input data
 IOHelper().inputFiles([
