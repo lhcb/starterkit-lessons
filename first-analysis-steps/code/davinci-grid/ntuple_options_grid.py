17100f856512755461fdc4e5dab911e598eb88da
Chris Burr <chrisburr@users.noreply.github.com>
2017-11-02 10:35:07 +0100

    Update DB Tags in grid options filew

--- first-analysis-steps/code/davinci-grid/ntuple_options_grid.py
+++ first-analysis-steps/code/davinci-grid/ntuple_options_grid.py
@@ -21,5 +21,5 @@ DaVinci().Simulation = True
 # Only ask for luminosity information when not using simulated data
 DaVinci().Lumi = not DaVinci().Simulation
 DaVinci().EvtMax = -1
-DaVinci().CondDBtag = 'sim-20170721-1-vc-mu100'
-DaVinci().DDDBtag = 'dddb-20171005-3'
+DaVinci().CondDBtag = 'sim-20161124-2-vc-md100'
+DaVinci().DDDBtag = 'dddb-20150724'
