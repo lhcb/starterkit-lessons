dd259d972dd3b094291d24850c05ecdc7e0edf49
Christopher Burr <christopher.burr@cern.ch>
2018-05-06 23:30:23 +0200

    Make Snakemake example follow PEP8

--- second-analysis-steps/code/snakemake/tutorial/solution/merge.py
+++ second-analysis-steps/code/snakemake/tutorial/solution/merge.py
@@ -1,13 +1,11 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-i","--intxt",nargs="+",required=True)
-parser.add_argument("-o","--outtxt",required=True)
+parser.add_argument("-i", "--intxt", nargs="+", required=True)
+parser.add_argument("-o", "--outtxt", required=True)
 args = parser.parse_args()
 
-outf = open(args.outtxt,"w")
-for f in args.intxt :
-    with open(f) as txt :
-        outf.write(txt.read()+"\n")
-
-outf.close() 
+with open(args.outtxt, "wt") as outf:
+    for f in args.intxt:
+        with open(f, "rt") as txt:
+            outf.write(txt.read()+"\n")
