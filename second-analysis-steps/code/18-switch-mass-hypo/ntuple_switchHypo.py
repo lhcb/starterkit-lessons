e208627bed18bb0fe437ec6923292263ae028fae
Alex Pearce <alex@alexpearce.me>
2018-05-08 10:50:28 +0000

    Rebuild pages at 434299f

--- second-analysis-steps/code/18-switch-mass-hypo/ntuple_switchHypo.py
+++ second-analysis-steps/code/18-switch-mass-hypo/ntuple_switchHypo.py
@@ -6,7 +6,7 @@ from DecayTreeTuple.Configuration import *
 
 # Stream and stripping line we want to use
 stream = 'AllStreams'
-line = 'D2hhCompleteEventPromptDst2D2RSLine'
+line = 'D2hhPromptDst2D2KKLine'
 tesLoc = '/Event/{0}/Phys/{1}/Particles'.format(stream, line)
 
 # get the selection(s) created by the stripping
@@ -15,18 +15,17 @@ strippingSels = [DataOnDemand(Location=tesLoc)]
 # configure an algorithm to substitute the Kaon in the D0-decay by a second
 # pion
 subs = SubstitutePID(
-    'MakeD02pipi',
-    Code="DECTREE('[D*(2010)+ -> (D0 -> K- pi+) pi+]CC')",
+    'MakeD02Kpi',
+    Code="DECTREE('[D*(2010)+ -> (D0 -> K- K+) pi+]CC')",
     # note that SubstitutePID can't handle automatic CC
     Substitutions={
-        'Charm -> (D0 -> ^K- pi+) Meson': 'pi-',
-        'Charm -> (D~0 -> ^K+ pi-) Meson': 'pi+'
-    }
+        'Charm -> (D0 -> K- ^K+) Meson': 'pi+',
+        'Charm -> (D~0 -> ^K- K+) Meson': 'pi-'}
 )
 
 # create a selection using the substitution algorithm
 selSub = Selection(
-    'Dst2D0pi_D02pipi_Sel',
+    'Dst2D0pi_D02Kpi_Sel',
     Algorithm=subs,
     RequiredSelections=strippingSels
 )
@@ -35,9 +34,9 @@ selSeq = SelectionSequence('SelSeq', TopSelection=selSub)
 
 
 # Create an ntuple to capture D*+ decays from the new selection
-dtt = DecayTreeTuple('TupleDstToD0pi_D0Topipi')
+dtt = DecayTreeTuple('TupleDstToD0pi_D0ToKpi')
 dtt.Inputs = [selSeq.outputLocation()]
-dtt.Decay = '[D*(2010)+ -> ^(D0 -> ^pi- ^pi+) ^pi+]CC'
+dtt.Decay = '[D*(2010)+ -> ^(D0 -> ^K- ^pi+) ^pi+]CC'
 
 # Configure DaVinci
 
@@ -50,7 +49,7 @@ DaVinci().appendToMainSequence([seq])
 DaVinci().InputType = 'DST'
 DaVinci().TupleFile = 'DVntuple.root'
 DaVinci().PrintFreq = 1000
-DaVinci().DataType = '2012'
+DaVinci().DataType = '2016'
 DaVinci().Simulation = True
 # Only ask for luminosity information when not using simulated data
 DaVinci().Lumi = not DaVinci().Simulation
@@ -58,5 +57,5 @@ DaVinci().EvtMax = -1
 
 # Use the local input data
 IOHelper().inputFiles([
-    './00035742_00000002_1.allstreams.dst'
+    './00062514_00000001_7.AllStreams.dst'
 ], clear=True)
