64d4bc2bb58e6467f7e3bc7486367a4267aeb791
Sascha Stahl <sascha.stahl@cern.ch>
2020-11-05 13:47:50 +0100

    More replacement of daughters and mother

--- first-analysis-steps/code/building-decays/build_decays.py
+++ first-analysis-steps/code/building-decays/build_decays.py
@@ -12,13 +12,13 @@ from Configurables import CombineParticles, DaVinci
 Pions = DataOnDemand('Phys/StdAllNoPIDsPions/Particles')
 Kaons = DataOnDemand('Phys/StdAllLooseKaons/Particles')
 
-d0_daughters = {
+d0_decay_products = {
     'pi-': '(PT > 750*MeV) & (P > 4000*MeV) & (MIPCHI2DV(PRIMARY) > 4)',
     'K+': '(PT > 750*MeV) & (P > 4000*MeV) & (MIPCHI2DV(PRIMARY) > 4)'
 }
 d0_comb = "(AMAXDOCA('') < 0.2*mm) & (ADAMASS('D0') < 100*MeV)"
 # We can split long selections across multiple lines
-d0_mother = (
+d0_vertex = (
     '(VFASPF(VCHI2/VDOF)< 9)'
     '& (BPVDIRA > 0.9997)'
     "& (ADMASS('D0') < 70*MeV)"
@@ -26,15 +26,15 @@ d0_mother = (
 
 d0 = CombineParticles('Combine_D0',
                       DecayDescriptor='([D0 -> pi- K+]CC)',
-                      DaughtersCuts=d0_daughters,
+                      DaughtersCuts=d0_decay_products,
                       CombinationCut=d0_comb,
-                      MotherCut=d0_mother)
+                      MotherCut=d0_vertex)
 
 d0_sel = Selection('Sel_D0',
                    Algorithm=d0,
                    RequiredSelections=[Pions, Kaons])
 
-dstar_daughters = {'pi+': '(TRCHI2DOF < 3) & (PT > 100*MeV)'}
+dstar_decay_products = {'pi+': '(TRCHI2DOF < 3) & (PT > 100*MeV)'}
 dstar_comb = "(ADAMASS('D*(2010)+') < 400*MeV)"
 dstar_mother = (
     "(abs(M-MAXTREE('D0'==ABSID,M)-145.42) < 10*MeV)"
@@ -45,7 +45,7 @@ dstar_sel = SimpleSelection(
     ConfigurableGenerators.CombineParticles,
     [d0_sel, Pions],
     DecayDescriptor='[D*(2010)+ -> D0 pi+]cc',
-    DaughtersCuts=dstar_daughters,
+    DaughtersCuts=dstar_decay_products,
     CombinationCut=dstar_comb,
     MotherCut=dstar_mother
 )
